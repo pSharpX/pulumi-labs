8c444798518411ba4c5e5e87c884dfbe61e53d02
Christian Rivera <crivera2093@gmail.com>
2024-02-03 21:27:33 -0500

    feat: changed index file

--- aws/ec2/cloudinit-ts/index.ts
+++ aws/ec2/cloudinit-ts/index.ts
@@ -1,10 +1,11 @@
 import { OneBankInstance } from "./app/OneBankInstance";
 import { OneBankKeyPair } from "./app/OneBankKeyPair";
 import { OneBankSecurityGroup } from "./app/OneBankSecurityGroup";
+import { Platform } from "./app/OneBankAmiResolver";
 
 const securityGroupResource = new OneBankSecurityGroup("dev-onebankapi");
 const keyPairResource = new OneBankKeyPair("dev-onebank");
-const instanceResource = new OneBankInstance("dev-onebankapi", keyPairResource.kp.id, [securityGroupResource.sg.id]);
+const instanceResource = new OneBankInstance("dev-onebankapi", Platform.WINDOWS_HOME, keyPairResource.kp.id, [securityGroupResource.sg.id]);
 
 // Export the name of the bucket
 export const instanceIp = instanceResource.instance.publicIp;
