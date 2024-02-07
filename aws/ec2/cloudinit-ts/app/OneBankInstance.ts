d8cdafc5f56248d70f02361a20a365f773be11e9
Christian Rivera <crivera2093@gmail.com>
2024-02-07 01:02:47 -0500

    feat: added cloudinit config for provision ec2 instance

--- aws/ec2/cloudinit-ts/app/OneBankInstance.ts
+++ aws/ec2/cloudinit-ts/app/OneBankInstance.ts
@@ -5,19 +5,17 @@ import { Platform, OneBankAmiResolver } from "./OneBankAmiResolver";
 export class OneBankInstance {
     readonly instance: aws.ec2.Instance;
 
-    constructor(resourceName: string, platform: Platform, keyPairId: pulumi.Input<string>, securityGroups: pulumi.Input<string>[]) {
+    constructor(resourceName: string, platform: Platform, keyPairId: pulumi.Input<string>, userData: pulumi.Input<string>, securityGroups: pulumi.Input<string>[]) {
         const config = new pulumi.Config();
         const tags = config.requireObject<object>("tags");
 
         const ami = new OneBankAmiResolver().resolve(platform).lookup();
         this.instance = new aws.ec2.Instance(`${resourceName}-instance`, {
-            ami: ami.then(ubuntu => {
-                console.log(ubuntu);
-                return ubuntu.id;
-            }),
+            ami: ami.then(ami => ami.id),
             instanceType: "t2.micro",
             keyName: keyPairId,
             vpcSecurityGroupIds: securityGroups,
+            //userData: userData,
             tags: {
                 ...tags,
                 Name: resourceName
--- aws/ec2/cloudinit-ts/index.ts
+++ aws/ec2/cloudinit-ts/index.ts
@@ -1,11 +1,15 @@
+import * as fs from "fs";
+import * as path from "path";
 import { OneBankInstance } from "./app/OneBankInstance";
 import { OneBankKeyPair } from "./app/OneBankKeyPair";
 import { OneBankSecurityGroup } from "./app/OneBankSecurityGroup";
 import { Platform } from "./app/OneBankAmiResolver";
 
+const userData = fs.readFileSync(path.join(__dirname, "./config/userdata.yml"), "utf8");
+
 const securityGroupResource = new OneBankSecurityGroup("dev-onebankapi");
 const keyPairResource = new OneBankKeyPair("dev-onebank");
-const instanceResource = new OneBankInstance("dev-onebankapi", Platform.AMAZON_LINUX, keyPairResource.kp.id, [securityGroupResource.sg.id]);
+const instanceResource = new OneBankInstance("dev-onebankapi", Platform.AL2023, keyPairResource.kp.id, userData, [securityGroupResource.sg.id]);
 
 // Export the identifiers of the resources
 export const instanceIp = instanceResource.instance.publicIp;
