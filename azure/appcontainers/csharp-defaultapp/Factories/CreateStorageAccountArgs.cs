a3c0e6c4fb543efef93ff99cf2175d882899199b
Christian Rivera <crivera2093@gmail.com>
2026-03-03 23:33:09 -0500

    feat(azure): new factory class for storage account creation.

--- azure/appcontainers/csharp-defaultapp/Factories/CreateStorageAccountArgs.cs
+++ azure/appcontainers/csharp-defaultapp/Factories/CreateStorageAccountArgs.cs
@@ -1,6 +1,17 @@
+using Pulumi;
+
 namespace defaultapp.Factories;
 
-public class CreateStorageAccountArgs
+public class CreateStorageAccountArgs: CreateResourceArgs
 {
-    
+    public Input<bool> AllowBlobPublicAccess { get; init; } = false;
+    public Input<bool> AllowSharedKeyAccess { get; init; } = false;
+    public bool ImmutableStorageEnabled { get; init; } = false;
+    public bool EncryptionEnabled { get; init; } = false;
+    public Input<bool> EnableHttpsTrafficOnly { get; init; } = true;
+    public Input<string> Kind { get; init; } = "StorageV2"; //AzureNative.Storage.Kind.Storage (Storage, StorageV2, BlobStorage, FileStorage, BlockBlobStorage)
+    public Input<string> PublicNetworkAccess { get; init; } = "Enabled"; //AzureNative.Storage.PublicNetworkAccess.Enabled
+    public Input<string> SkuName { get; init; } =  "Standard_GRS"; // AzureNative.Storage.SkuName.Standard_GRS
+    public string AccessTier { get; init; } = "Hot"; //AzureNative.Storage.AccessTier.Hot
+    public Input<string> MinimumTlsVersion { get; init; } = "TLS1_2"; //AzureNative.Storage.MinimumTlsVersion.TLS1_2 (TLS1_0, TLS1_1, TLS1_2, TLS1_3)
 }
--- azure/appcontainers/csharp-defaultapp/Factories/StorageAccountFactory.cs
+++ azure/appcontainers/csharp-defaultapp/Factories/StorageAccountFactory.cs
@@ -1,6 +1,40 @@
+using System;
+using Pulumi;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+
 namespace defaultapp.Factories;
 
-public class StorageAccountFactory
+public static class StorageAccountFactory
 {
-    
+    public static StorageAccount Create(CreateStorageAccountArgs args)
+    {
+        ImmutableStorageAccountArgs? immutableStorageSettings = null;
+        if (args.ImmutableStorageEnabled)
+        {
+            immutableStorageSettings = new ImmutableStorageAccountArgs
+            {
+                Enabled =  true,
+            };
+        }
+        return new StorageAccount("OneBank_StorageAccount", new StorageAccountArgs
+        {
+            AccountName =  args.Name,
+            ResourceGroupName = args.ResourceGroupName,
+            Location = args.Location,
+            Kind = args.Kind,
+            Sku = new SkuArgs
+            {
+                Name = args.SkuName,
+            },
+            MinimumTlsVersion = args.MinimumTlsVersion,
+            EnableHttpsTrafficOnly = args.EnableHttpsTrafficOnly,
+            AccessTier =  Enum.Parse<AccessTier>(args.AccessTier),
+            PublicNetworkAccess = args.PublicNetworkAccess,
+            AllowBlobPublicAccess = args.AllowBlobPublicAccess,
+            AllowSharedKeyAccess = args.AllowSharedKeyAccess,
+            ImmutableStorageWithVersioning = immutableStorageSettings!,
+            Tags = args.Tags!,
+        }, new CustomResourceOptions{ Parent = args.Parent });
+    }
 }
