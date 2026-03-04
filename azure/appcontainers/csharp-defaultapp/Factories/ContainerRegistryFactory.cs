1bff6e75d672d45bc60543eff99f866fb4c69bd0
Christian Rivera <crivera2093@gmail.com>
2026-03-03 23:32:49 -0500

    feat(azure): new factory class for registry creation.

--- azure/appcontainers/csharp-defaultapp/Factories/ContainerRegistryFactory.cs
+++ azure/appcontainers/csharp-defaultapp/Factories/ContainerRegistryFactory.cs
@@ -1,6 +1,41 @@
+using Pulumi;
+using Pulumi.AzureNative.ContainerRegistry;
+using Pulumi.AzureNative.ContainerRegistry.Inputs;
+
 namespace defaultapp.Factories;
 
-public class ContainerRegistryFactory
+public static class ContainerRegistryFactory
 {
-    
+    public static Registry Create(CreateContainerRegistryArgs args)
+    {
+        EncryptionPropertyArgs? encryptionSettings = null;
+        if (args.EncryptionEnabled)
+        {
+            encryptionSettings = new EncryptionPropertyArgs
+            {
+                KeyVaultProperties = new KeyVaultPropertiesArgs
+                {
+                    Identity =  args.VaultIdentity,
+                    KeyIdentifier =  args.VaultKeyIdentifier,
+                },
+                Status = EncryptionStatus.Enabled,
+            };
+        }
+        return new Registry("OneBank_ContainerRegistry", new RegistryArgs()
+        {
+            RegistryName = args.Name,
+            ResourceGroupName = args.ResourceGroupName,
+            Location = args.Location,
+            Sku = new SkuArgs
+            {
+                Name = args.SkuName,
+            },
+            AdminUserEnabled =  args.AdminUserEnabled,
+            PublicNetworkAccess =  args.PublicNetworkAccess,
+            NetworkRuleBypassOptions =  args.NetworkRuleBypassOptions,
+            AnonymousPullEnabled =   args.AnonymousPullEnabled,
+            Encryption = encryptionSettings!,
+            Tags = args.Tags!,
+        }, new CustomResourceOptions{ Parent = args.Parent });
+    }
 }
--- azure/appcontainers/csharp-defaultapp/Factories/CreateContainerRegistryArgs.cs
+++ azure/appcontainers/csharp-defaultapp/Factories/CreateContainerRegistryArgs.cs
@@ -1,6 +1,19 @@
+using Pulumi;
+
 namespace defaultapp.Factories;
 
 public class CreateContainerRegistryArgs: CreateResourceArgs
 {
+    public Input<bool> AdminUserEnabled { get; init; } = false;
+    public Input<string> NetworkRuleBypassOptions { get; init; } = "";
+    public Input<string> SkuName { get; init; } = "Standard"; // Classic, Basic, Standard, Premium
+    public Input<string> PublicNetworkAccess { get; init; } = "Enabled"; 
+    public Input<bool> AnonymousPullEnabled { get; init; } = false;
+    public bool EncryptionEnabled { get; init; } = false;
+    public Input<string>? VaultIdentity { get; init; }
+    public Input<string>? VaultKeyIdentifier { get; init; }
     
+    //Identity
+    //NetworkRuleSet
+    //Policies
 }
