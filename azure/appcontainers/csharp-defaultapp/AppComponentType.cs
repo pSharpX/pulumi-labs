ea27915d482ea4f60fed604f6e97afe8823a70a4
Christian Rivera <crivera2093@gmail.com>
2026-03-04 19:27:55 -0500

    feat: add new app-component type.

--- azure/appcontainers/csharp-defaultapp/AppComponentType.cs
+++ azure/appcontainers/csharp-defaultapp/AppComponentType.cs
@@ -2,10 +2,12 @@ namespace defaultapp;
 
 public enum AppComponentType
 {
-    DefaultApp, // application running on container apps
+    DefaultApp, // containerized application running on container apps
     EnterpriseApp, // application running on a microservice architecture/pattern 
     LegacyApp, // application running on virtual machine
     ServiceApp, // application running on aks
     WebApp, // long-lived application running on app service
+    StaticWebApp, // Static application hosted on Storage Account  
     FunctionApp, // serverless application or function
+    WorkerApp, // run-to-completion application running on container instance
 }
